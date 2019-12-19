b44acfd8f05d6e44b98f0110c1ee925b124a7cdf
ittyBittyBytes <loralie.flint0128@gmail.com>
2019-12-18 18:02:01 -0700

    Get requests finished

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-const express = require('express');
-
-const db = require('./data/dbConfig.js');
-
+const express = require("express");
+const db = require("./data/dbConfig");
 const server = express();
-
 server.use(express.json());
+
+server.get("/", (req, res) => {
+  res.send(`
+    <h1>Lambda webdb-i-challenge!</h1>
+  `);
+});
+
+server.get("/budget", async (req, res, next) => {
+  try {
+    res.json(await db("accounts").select('budget'));
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+server.get("/budget/:id", async (req, res, next) => {
+    try {
+        res.json(await db("accounts").where("id", req.params.id).first("budget"))
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  });
+
+
 
 module.exports = server;
